0b93c4631ac3a97aea1868ebd3627adea4fbf5fe
alison <alison-bh@hotmail.com>
2024-01-11 09:36:40 -0300

    finaliza component footer

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from '../../components/banner/banner.component';
 import { ContainerComponent } from '../../components/container/container.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BannerComponent, ContainerComponent],
+  imports: [BannerComponent, ContainerComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
